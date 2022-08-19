8e768943e930f381f06c18c67368d14b2167a5fc
Gebi <andreigebaila@gmail.com>
2022-08-19 04:36:11 +0100

    Added 2 ways of calculating node cost

--- src/Node.java
+++ src/Node.java
@@ -3,18 +3,21 @@ import java.util.ArrayList;
 public class Node implements Comparable<Node>{
     State state;
     Node parent;
-    private final int cost;
+    int cost;
     private final int depth;
 
-    public Node(State state, Node parent, int cost, int depth) {
-        this.state = state;
-        this.parent = parent;
-        this.cost = cost;
-        this.depth = depth;
-    }
     // Used for the start node
     public Node(State state) {
-        this(state,null,0, 0);
+        this(state,null, 0);
+    }
+
+    public Node(State state, Node parent, int depth) {
+        this.state = state;
+        this.parent = parent;
+        this.depth = depth;
+        // if(manhattan heuristics) do this
+        cost = (int)Math.pow(state.GetHeuristics(), 4) * depth;
+        //cost = state.GetHeuristics() + depth;
     }
 
     public int getDepth(){
@@ -22,12 +25,13 @@ public class Node implements Comparable<Node>{
     }
     @Override
     public int compareTo(Node o) {
+        //return Integer.compare((int)(Math.pow(this.state.GetHeuristics(), 4) * this.depth), (int)(Math.pow(o.state.GetHeuristics(), 4) * o.getDepth()));
         return Integer.compare(this.cost, o.cost);
     }
-
+    // Find state in array of nodes
     public static Node findNodeWithState(ArrayList<Node> nodeList, State gs) {
         for (Node n : nodeList) {
-            if (gs.sameBoard(n.state)) return n;
+            if (gs.SameBoard(n.state)) return n;
         }
         return null;
     }
